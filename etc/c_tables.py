62b4be35a7d682567976eb92da6f382f6d4d5d6f
Rahul Gopinath <rahul@gopinath.org>
2020-06-03 00:53:20 -0700

    update

--- etc/c_tables.py
+++ etc/c_tables.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+import glob
 def precision_text(txt):
     val, total = re.sub('.timeout:.*$', '', re.sub('^.*result: ', '',txt)).split('/')
     return str(int(val) * 100.0 / int(total)) + '%'
@@ -13,15 +14,17 @@ def main():
     # Precision
     print("\t".join(["Precision (Table 1)", "Mimid"]))
     print('-----------------')
-    for key in projects:
-        with open("/home/vagrant/mimid/Cmimid/build/%s.precision" % key) as f:
+    for p in glob.glob('/home/vagrant/mimid/Cmimid/build/*.precision'):
+        key = p.replace('/home/vagrant/mimid/Cmimid/build/','').replace('.precision', '')
+        with open(p) as f:
             print(key + "\t" + precision_text(f.read().strip()))
     print()
     # fuzz
     print("\t".join(["Recall (Table 2)", "Mimid"]))
     print('-----------------')
-    for key in projects:
-        with open("/home/vagrant/mimid/Cmimid/build/%s.fuzz" % key) as f:
+    for p in glob.glob('/home/vagrant/mimid/Cmimid/build/*.fuzz'):
+        key = p.replace('/home/vagrant/mimid/Cmimid/build/','').replace('.fuzz', '')
+        with open(p) as f:
             print(key + "\t" + recall_text(f.read().strip()))
 
 main()
